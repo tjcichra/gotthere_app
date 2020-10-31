086d71d722699849b40c354079e912d21e86841a
Tim <tjcichra@yahoo.com>
2020-10-31 10:32:30 -0400

    v0.1 release. It will try to connect to the server and reconnect if it can't or loses connection afterward. It stores locations in the queue and will send them all out when it receives connection. When the app is not visible, a notification will appear and say if the app is connected or not.

--- app/src/main/java/com/tim/gotthere_app/LocationService.java
+++ app/src/main/java/com/tim/gotthere_app/LocationService.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -49,8 +50,8 @@ public class LocationService extends Service {
 	public static final String EXTRA_LOCATION = PACKAGE_NAME + ".location";
 	public static final String EXTRA_STARTED_FROM_NOTIFICATION = PACKAGE_NAME + ".started_from_notification";
 
-	private final long INTERVAL = 5000;
-	private final long FASTEST_INTERVAL = 2500;
+	private final long INTERVAL = 30000;
+	private final long FASTEST_INTERVAL = 15000;
 
 	private static final int NOTIFICATION_ID = 12345678;
 
@@ -67,6 +68,13 @@ public class LocationService extends Service {
 
 	private BlockingQueue<Location> locationQueue = new ArrayBlockingQueue<>(512);
 
+	private boolean isConnected = false;
+	private boolean lostConnection = false;
+	private boolean closing = false;
+
+	private Thread socketThread = new Thread(this::connectSocket);
+	private Thread locationThread = new Thread(this::readLocationQueue);
+
 	public class LocalBinder extends Binder {
 		LocationService getService() {
 			return LocationService.this;
@@ -80,22 +88,127 @@ public class LocationService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Log.d(TAG, "onCreate");
+		Log.d(TAG, "onCreate()");
 
-		new Thread(this.socketConnection).start();
-
-		new Thread(this::datathingy).start();
+		//Start thread for connection
+		this.socketThread.start();
+		//Start thread for reading queued locations.
+		this.locationThread.start();
 
 		Log.d(TAG, "Ready");
 
+		this.setupFusedLocationClient();
+	}
+
+	/**
+	 * Called after the service is bound to the main activity (but not when "promoted" to the foreground).
+	 */
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "onStartCommand()");
+		boolean startedFromNotification = intent.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION, false);
+
+		if(startedFromNotification) {
+			this.removeLocationUpdates();
+			this.stopSelf();
+		}
+
+		//Returns START_STICKY so that when memory is needed it doesn't kill the service.
+		return START_STICKY;
+	}
+
+	/**
+	 * Called when the main activity binds to the service with bindService() (called before startCommand()).
+	 * Stops this service on the foreground and returns an interface used to interact with the service.
+	 */
+	@Nullable
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i(TAG, "onBind()");
+		this.stopForeground(true);
+		this.mChangingConfiguration = false;
+		return this.M_BINDER;
+	}
+
+	/**
+	 * Called when the main activity rebinds to the service after unbinding (called before startCommand())
+	 */
+	@Override
+	public void onRebind(Intent intent) {
+		// Called when a client (MainActivity in case of this sample) returns to the foreground
+		// and binds once again with this service. The service should cease to be a foreground
+		// service when that happens.
+		Log.i(TAG, "onRebind()");
+		this.stopForeground(true);
+		mChangingConfiguration = false;
+		super.onRebind(intent);
+	}
+
+	/**
+	 *
+	 * @param intent
+	 * @return
+	 */
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Log.i(TAG, "onUnbind()");
+
+		// Called when the last client (MainActivity in case of this sample) unbinds from this
+		// service. If this method is called due to a configuration change in MainActivity, we
+		// do nothing. Otherwise, we make this service a foreground service.
+		if (!mChangingConfiguration && Util.requestingLocationUpdates(this)) {
+			Log.i(TAG, "Starting foreground service");
+
+			this.startForeground(NOTIFICATION_ID, this.getNotification());
+		}
+		return true; // Ensures onRebind() is called when a client re-binds.
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		this.mChangingConfiguration = true;
+	}
+
+	/**
+	 * Called when the foreground service is stopped and there are no bindings (ex. when the app itself is stopped).
+	 * This is for cleaning up sockets and threads.
+	 */
+	@Override
+	public void onDestroy() {
+		Log.i(TAG, "onDestroy()");
+		this.closing = true;
+		this.locationThread.interrupt();
+		try {
+			this.locationThread.join();
+			this.socketThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		if(this.socket != null) {
+			try {
+				this.socket.getInputStream().close();
+				this.socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		mServiceHandler.removeCallbacksAndMessages(null);
+	}
+
+	/**
+	 * Used for setting up the fused location client, which will start to insert locations into the location queue.
+	 */
+	public void setupFusedLocationClient() {
 		this.mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+		//Set what happens when a new location is retrieved.
 		this.mLocationCallback = new LocationCallback() {
 			@Override
 			public void onLocationResult(LocationResult locationResult) {
 				super.onLocationResult(locationResult);
 				onNewLocation(locationResult.getLastLocation());
-				Log.d(TAG, "" + locationResult.getLastLocation().getLongitude());
 			}
 		};
 
@@ -112,80 +225,52 @@ public class LocationService extends Service {
 
 			mNotificationManager.createNotificationChannel(mChannel);
 		}
-	}
 
-	/**
-	 * Called when the service is "promoted" to the foreground.
-	 */
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.i(TAG, "Service started");
-		boolean startedFromNotification = intent.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION, false);
-
-		if(startedFromNotification) {
-			this.removeLocationUpdates();
-			this.stopSelf();
-		}
-
-		return START_NOT_STICKY;
-	}
-
-	/**
-	 * Called when the main activity binds to the services with bindService().
-	 * Stops this service on the foreground and returns an interface used to interact with the service.
-	 */
-	@Nullable
-	@Override
-	public IBinder onBind(Intent intent) {
-		Log.i(TAG, "in onBind()");
-		this.stopForeground(true);
-		this.mChangingConfiguration = false;
-		return this.M_BINDER;
-	}
-
-	@Override
-	public void onRebind(Intent intent) {
-		// Called when a client (MainActivity in case of this sample) returns to the foreground
-		// and binds once again with this service. The service should cease to be a foreground
-		// service when that happens.
-		Log.i(TAG, "in onRebind()");
-		this.stopForeground(true);
-		mChangingConfiguration = false;
-		super.onRebind(intent);
-	}
-
-	@Override
-	public boolean onUnbind(Intent intent) {
-		Log.i(TAG, "Last client unbound from service");
-
-		// Called when the last client (MainActivity in case of this sample) unbinds from this
-		// service. If this method is called due to a configuration change in MainActivity, we
-		// do nothing. Otherwise, we make this service a foreground service.
-		if (!mChangingConfiguration && Util.requestingLocationUpdates(this)) {
-			Log.i(TAG, "Starting foreground service");
-
-			this.startForeground(NOTIFICATION_ID, getNotification());
-		}
-		return true; // Ensures onRebind() is called when a client re-binds.
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		this.mChangingConfiguration = true;
-	}
-
-	@Override
-	public void onDestroy() {
-		if(this.socket != null) {
-			try {
-				this.socket.getInputStream().close();
-				this.socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+		/*HandlerThread handlerThread1 = new HandlerThread(TAG + "2");
+		handlerThread1.start();
+		Handler handler = new Handler(handlerThread1.getLooper());
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				Log.d(TAG,"Tanner is real");
 			}
+		});*/
+	}
+
+	private void createLocationRequest() {
+		this.mLocationRequest = new LocationRequest();
+		this.mLocationRequest.setInterval(this.INTERVAL);
+		this.mLocationRequest.setFastestInterval(this.FASTEST_INTERVAL);
+		this.mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+	}
+
+	private void onNewLocation(Location location) {
+		Log.i(TAG, "New location: " + location);
+
+		mLocation = location;
+
+		try {
+			this.locationQueue.put(location);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		mServiceHandler.removeCallbacksAndMessages(null);
+	}
+
+	private void getLastLocation() {
+		try {
+			this.mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+				@Override
+				public void onComplete(@NonNull Task<Location> task) {
+					if (task.isSuccessful() && task.getResult() != null) {
+						mLocation = task.getResult();
+					} else {
+						Log.w(TAG, "Failed to get location.");
+					}
+				}
+			});
+		} catch (SecurityException unlikely) {
+			Log.e(TAG, "Lost location permission." + unlikely);
+		}
 	}
 
 	/**
@@ -231,40 +316,56 @@ public class LocationService extends Service {
 				.apply();
 	}
 
-	Runnable socketConnection = () -> {
-		boolean connected = false;
-
-		while(!connected) {
+	/**
+	 * Used to connect the socket to the GotThere server. It does not stop until connection is established.
+	 * Should be ran as a separate thread.
+	 */
+	public void connectSocket() {
+		while(!closing && this.socket == null) {
 			try {
 				this.socket = new Socket("10.0.0.224", 2810);
-				connected = true;
-			} catch (Exception e) {
+				
+			} catch (UnknownHostException ex) {
+				//This is ran when the host could not be determined.
+				ex.printStackTrace();
+			} catch(IOException e) {
+				//This is ran when the socket fails to connect.
 				Log.d(TAG, e.getMessage());
+
+				//Sleeps for 10 seconds before retrying
 				try {
 					Thread.sleep(10000);
-				} catch (InterruptedException ex) {
+				} catch(InterruptedException ex) {
 					ex.printStackTrace();
 				}
+			} catch(SecurityException ex) {
+				//This is ran when the socket runs into security issues.
+				ex.printStackTrace();
+			} catch(IllegalArgumentException ex) {
+				//This is ran when the port number is invalid (not in-between 0 and 65535)
+				ex.printStackTrace();
 			}
 		}
 
-		Log.d(TAG, "Connected");
-	};
+		if(!closing) {
+			Log.d(TAG, "Connected");
+		}
 
-	private void onNewLocation(Location location) {
-		Log.i(TAG, "New location: " + location);
+		this.lostConnection = false;
+		this.isConnected = true;
 
-		mLocation = location;
-
-		try {
-			this.locationQueue.put(location);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		// Update notification content if running as a foreground service.
+		if (!closing && this.serviceIsRunningInForeground(this)) {
+			mNotificationManager.notify(NOTIFICATION_ID, getNotification());
 		}
 	}
 
-	public void datathingy() {
-		while(true) {
+	/**
+	 * Used to send out locations from the location queue.
+	 * Should be ran as a separate thread.
+	 */
+	public void readLocationQueue() {
+		while(!closing) {
 			if (this.socket != null && this.socket.isConnected()) {
 				try {
 					Location location = this.locationQueue.take();
@@ -284,19 +385,24 @@ public class LocationService extends Service {
 						Intent intent = new Intent(ACTION_BROADCAST);
 						intent.putExtra(EXTRA_LOCATION, location);
 						//LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
-
-						// Update notification content if running as a foreground service.
-						if (this.serviceIsRunningInForeground(this)) {
-							//mNotificationManager.notify(NOTIFICATION_ID, getNotification());
-						}
-					} catch (SocketException e) {
+					} catch(SocketException e) {
 						try {
+							this.lostConnection = true;
+
 							this.locationQueue.offer(location);
 							this.socket.getOutputStream().close();
 							this.socket.close();
 							this.socket = null;
 
-							new Thread(this.socketConnection).start();
+							// Update notification content if running as a foreground service.
+							if (this.serviceIsRunningInForeground(this)) {
+								mNotificationManager.notify(NOTIFICATION_ID, getNotification());
+							}
+
+							if(!this.closing) {
+								this.socketThread = new Thread(this::connectSocket);
+								this.socketThread.start();
+							}
 						} catch (IOException ex) {
 							ex.printStackTrace();
 						}
@@ -304,7 +410,7 @@ public class LocationService extends Service {
 						e.printStackTrace();
 					}
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+
 				}
 			}
 		}
@@ -340,9 +446,9 @@ public class LocationService extends Service {
 			buffer[start + 1] = (byte) value;
 		}
 
-		int fvalue = ((int) (Math.abs(value) * 100)) % 100;
-		int fvalue2 = ((int) (Math.abs(value) * 10000)) % 100;
-		int fvalue3 = ((int) (Math.abs(value) * 1000000)) % 100;
+		int fvalue = ((int) (value * 100)) % 100;
+		int fvalue2 = ((int) (value * 10000)) % 100;
+		int fvalue3 = ((int) (value * 1000000)) % 100;
 
 		buffer[start + 2] = (byte) fvalue;
 		buffer[start + 3] = (byte) fvalue2;
@@ -351,7 +457,7 @@ public class LocationService extends Service {
 
 	public void insertDoubleTwo(byte[] buffer, int start, double value) {
 		int ivalue = (int) value;
-		buffer[start] = (byte) value;
+		buffer[start] = (byte) ivalue;
 
 		int fvalue = ((int) (value * 100)) % 100;
 		buffer[start + 1] = (byte) fvalue;
@@ -359,8 +465,15 @@ public class LocationService extends Service {
 
 	private Notification getNotification() {
 		Intent intent = new Intent(this, LocationService.class);
+		String message;
 
-		CharSequence text = MainActivity.getLocationText(mLocation);
+		if(this.lostConnection) {
+			message = "Lost connection. Trying to reconnect.";
+		} else if(this.isConnected) {
+			message = "Connected to server.";
+		} else {
+			message = "Trying to connect to server.";
+		}
 
 		// Extra to help us figure out if we arrived in onStartCommand via the notification or not.
 		intent.putExtra(EXTRA_STARTED_FROM_NOTIFICATION, true);
@@ -374,38 +487,14 @@ public class LocationService extends Service {
 				new Intent(this, MainActivity.class), 0);
 
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-				.setContentText(text)
+				.setContentTitle(message)
 				.setOngoing(true)
 				.setPriority(Notification.PRIORITY_HIGH)
 				.setSmallIcon(R.mipmap.ic_launcher)
-				.setTicker(text)
+				.setTicker("Tim Ticker Text")
 				.setWhen(System.currentTimeMillis());
 
 		return builder.build();
-	}
-
-	private void getLastLocation() {
-		try {
-			this.mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-				@Override
-				public void onComplete(@NonNull Task<Location> task) {
-					if (task.isSuccessful() && task.getResult() != null) {
-						mLocation = task.getResult();
-					} else {
-						Log.w(TAG, "Failed to get location.");
-					}
-				}
-			});
-		} catch (SecurityException unlikely) {
-			Log.e(TAG, "Lost location permission." + unlikely);
-		}
-	}
-
-	private void createLocationRequest() {
-		this.mLocationRequest = new LocationRequest();
-		this.mLocationRequest.setInterval(this.INTERVAL);
-		this.mLocationRequest.setFastestInterval(this.FASTEST_INTERVAL);
-		this.mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 	}
 
 	public boolean serviceIsRunningInForeground(Context context) {
